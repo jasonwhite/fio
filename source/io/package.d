00731a0ce2ca21f48783a0ff9add02f5610d3b77
Jason White <jasonaw0@gmail.com>
2014-07-01 20:38:32 -0700

    Remove invalid source code link.

--- source/io/package.d
+++ source/io/package.d
@@ -2,7 +2,6 @@
  * Copyright: Copyright Jason White, 2013-
  * License:   $(WEB boost.org/LICENSE_1_0.txt, Boost License 1.0).
  * Authors:   Jason White
- * Source:    $(PHOBOSSRC std/_io/_package.d)
  */
 //module io.package;
 
