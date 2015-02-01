badd0c24e390c2d427d8bd0d5c2ee5f4c6056f58
Jason White <jasonaw0@gmail.com>
2015-02-01 01:33:44 -0800

    Add preliminary text writing.

--- source/io/text.d
+++ source/io/text.d
@@ -2,118 +2,110 @@
  * Copyright: Copyright Jason White, 2013-
  * License:   $(WEB boost.org/LICENSE_1_0.txt, Boost License 1.0).
  * Authors:   Jason White
+ *
+ * TODO: Find a more elegant way of reading and writing text.
  */
 module io.text;
-
-version (none):
 
 import io.stream;
 
 
 /**
- * Wraps a stream such that reads and writes are deferred for as long as
- * possible.
+ * Serializes the given arguments to a text representation followed by a new
+ * line.
  */
-struct TextStream(S)
-    if (isSource!S || isSink!S)
+size_t writeln(T...)(Sink sink, auto ref T args)
 {
-    S stream;
-    alias stream this;
+    import std.conv : to;
 
-    @disable this(this);
+    size_t length;
 
-    this(S stream)
-    {
-        import std.algorithm : move;
-        this.stream = move(stream);
-    }
+    foreach (arg; args)
+        length += sink.write(arg.to!string);
 
-    // Call super class constructors.
-    /*this(Args...)(auto ref Args args)
-        if(is(typeof(S(args))))
-    {
-        _stream = S(args);
-    }*/
-
-    static if (isSource!S)
-    {
-        size_t readf(Char, T...)(in Char[] fmt, T args)
-        {
-            // TODO
-            return 0;
-        }
-
-        size_t readln(Char)(ref Char[] buf, dchar terminator = '\n')
-        {
-            // TODO
-            return 0;
-        }
-
-        /**
-          Returns a range that iterates over the lines in a stream.
-         */
-        version (none)
-        @property auto byDelimiter(Terminator = char, Char = char)
-            (bool keepTerm = false, Terminator t = '\n')
-        {
-            struct ByDelimiter
-            {
-                bool empty()
-                {
-                    return true;
-                }
-
-                const(Char[]) front()
-                {
-                    return null;
-                }
-
-                void popFront()
-                {
-                }
-            }
-
-            return ByDelimiter();
-        }
-    }
-
-    static if (isSink!S)
-    {
-        size_t write(T...)(T args)
-        {
-            // TODO
-            return 0;
-        }
-
-        size_t writeln(T...)(T args)
-        {
-            // TODO
-            return 0;
-        }
-
-        size_t writef(Char, T...)(in Char[] fmt, T args)
-        {
-            // TODO
-            return 0;
-        }
-
-        size_t writefln(Char, T...)(in Char[] fmt, T args)
-        {
-            // TODO
-            return 0;
-        }
-    }
+    length += sink.write("\n");
+    return length;
 }
 
-TextStream!S text(S)(S stream)
-    if (isSource!S || isSink!S)
+/// Ditto
+size_t writeln(T...)(auto ref T args)
+    if (T.length > 0 && !is(T[0] : Sink))
 {
-    import std.algorithm : move;
-    return TextStream!S(move(stream));
+    import io.file.stdio : stdout;
+    import std.algorithm : forward;
+    return stdout.writeln(forward!args);
 }
 
 unittest
 {
-    alias S = TextStream!NullStream;
-    static assert(isSource!S && isSink!S);
+    import io.file.pipe;
+    import std.typecons : tuple;
+    import std.typetuple : TypeTuple;
+
+    // First tuple value is expected output. Remaining are the types to be
+    // printed.
+    alias tests = TypeTuple!(
+        tuple("\n"),
+        tuple("Test\n", "Test"),
+        tuple("[4, 8, 15, 16, 23, 42]\n", [4, 8, 15, 16, 23, 42]),
+        tuple("The answer is 42\n", "The answer is ", 42),
+        tuple("01234567890\n", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0),
+        );
+
+    foreach (t; tests)
+    {
+        auto f = pipe();
+        immutable output = t[0];
+        char[output.length] buf;
+        assert(f.writeEnd.writeln(t[1 .. $]) == output.length);
+        assert(f.readEnd.read(buf) == buf.length);
+        assert(buf == output);
+    }
+
+    /*auto f = pipe();
+
+    immutable output = "The answer is [4, 8, 15, 16, 23, 42]\n";
+
+    assert(f.writeEnd.writeln("The answer is ", [4, 8, 15, 16, 23, 42]) == output.length);
+
+    char[output.length] buf;
+    assert(f.readEnd.read(buf) == buf.length);
+    assert(buf == output);*/
+}
+
+/**
+ * Serializes the given arguments according to the given format specifier
+ * string.
+ */
+@property size_t writef(T...)(Sink sink, string format, auto ref T args)
+{
+    // TODO
+    return 0;
+}
+
+/// Ditto
+@property size_t writef(T...)(string format, auto ref T args)
+    if (T.length > 0 && !is(T[0] : Sink))
+{
+    import io.file.stdio : stdout;
+    import std.algorithm : forward;
+    return stdout.writef(forward!(format, args));
+}
+
+/**
+ * Like $(D writef), but also writes a new line.
+ */
+@property size_t writefln(T...)(Sink sink, string format, auto ref T args)
+{
+    // TODO
+    return 0;
+}
+
+/// Ditto
+@property size_t writefln(T...)(string format, auto ref T args)
+    if (T.length > 0 && !is(T[0] : Sink))
+{
+    import io.file.stdio : stdout;
+    import std.algorithm : forward;
+    return stdout.writefln(forward!(format, args));
 }
