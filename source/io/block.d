8799ac0e1848a6e2b662665bc54856f833560220
Jason White <jasonaw0@gmail.com>
2015-02-06 03:31:54 -0800

    Move byDelimiter io.text to io.range

--- source/io/block.d
+++ source/io/block.d
@@ -24,6 +24,8 @@ struct ByChunk
         size_t _valid;
     }
 
+    @disable this(this);
+
     this(Source source, size_t size = 4096)
     {
         this(source, new void[](size));
@@ -103,7 +105,18 @@ unittest
  */
 struct ByBlock(T)
 {
-    private Source _source;
+    private
+    {
+        Source _source;
+
+        // The current block in the stream.
+        T _current;
+
+        // Are we there yet?
+        bool _empty = false;
+    }
+
+    //@disable this(this);
 
     this(Source source)
     {
@@ -111,15 +124,6 @@ struct ByBlock(T)
 
         // Prime the cannons.
         popFront();
-    }
-
-    private
-    {
-        // The current block in the stream.
-        T _current;
-
-        // Are we there yet?
-        bool _empty = false;
     }
 
     /**
@@ -189,10 +193,213 @@ unittest
     f.write(data);
     f.position = 0;
 
-    auto blocks = f.byBlock!Data;
+    assert(equal(data, f.byBlock!Data));
+    assert(f.byBlock!Data.empty);
+}
 
-    assert(equal(data, blocks));
-    blocks.popFront();
-    assert(blocks.empty);
-    assert(!equal(data, blocks));
+/*
+ * Checks if the given delimiter is a valid delimiter for an element of type T.
+ */
+template isValidDelimiter(Delimiter, T)
+{
+    import std.traits : isScalarType, isArray, Unqual;
+    import std.range : ElementEncodingType;
+
+    static if (isScalarType!Delimiter)
+    {
+        enum isValidDelimiter = true;
+    }
+    else static if (isArray!Delimiter)
+    {
+        static if (is(Unqual!(ElementEncodingType!Delimiter) == T))
+            enum isValidDelimiter = true;
+        else
+            enum isValidDelimiter = false;
+    }
+    else
+        enum isValidDelimiter = false;
+}
+
+unittest
+{
+    static assert( isValidDelimiter!(char, char));
+    static assert( isValidDelimiter!(string, char));
+    static assert( isValidDelimiter!(dstring, dchar));
+    static assert(!isValidDelimiter!(dstring, wchar));
+    static assert(!isValidDelimiter!(dstring, char));
+    static assert(!isValidDelimiter!(wstring, char));
+    static assert( isValidDelimiter!(dchar, char));
+    static assert( isValidDelimiter!(int, char));
+    static assert( isValidDelimiter!(short, int));
+}
+
+struct ByDelimiter(T, Delimiter)
+    if (isValidDelimiter!(Delimiter, T))
+{
+    private
+    {
+        import std.array : Appender;
+        import io.block : ByBlock, byBlock;
+
+        // Holds the current line
+        Appender!(T[]) _line;
+
+        // Iterates over the stream in small blocks
+        ByBlock!T _blocks;
+
+        // Are we there yet?
+        bool _empty = false;
+
+        // Character or sequence of characters that terminates a line.
+        immutable Delimiter _delimiter;
+    }
+
+    @disable this(this);
+
+    this(Source source, Delimiter delimiter)
+    {
+        _blocks = source.byBlock!T;
+        _delimiter = delimiter;
+
+        // Prime the cannons
+        popFront();
+    }
+
+    /*
+     * Finds the length of the delimiter relative to the size of a single
+     * element in the line.
+     */
+    private @property size_t delimiterLength() const pure nothrow
+    {
+        import std.traits : isScalarType, isArray;
+
+        static if (isScalarType!Delimiter)
+        {
+            return 1;
+        }
+        else static if (isArray!Delimiter)
+        {
+            return _delimiter.length;
+        }
+        else
+        {
+            static assert("Unable to find length of line delimiter");
+        }
+    }
+
+    /**
+     * Reads the next line.
+     */
+    void popFront()
+    {
+        import std.algorithm : endsWith;
+
+        version(assert)
+        {
+            import core.exception : RangeError;
+            if (empty) throw new RangeError();
+        }
+
+        _line.clear();
+
+        if (_blocks.empty)
+        {
+            _empty = true;
+            return;
+        }
+
+        foreach (immutable ch; _blocks)
+        {
+            _line.put(ch);
+
+            if (_line.data.endsWith(_delimiter))
+            {
+                // Truncate the line to not include the delimiter
+                // FIXME: Handle arrays and ranges of delimiters
+                _line.shrinkTo(_line.data.length - delimiterLength);
+                break;
+            }
+        }
+
+        // popFront is not called when the loop exits, so we call it here.
+        _blocks.popFront();
+    }
+
+    /**
+     * Gets the current line in the stream.
+     */
+    const(T)[] front()
+    {
+        version(assert)
+        {
+            import core.exception : RangeError;
+            if (empty) throw new RangeError();
+        }
+
+        return _line.data;
+    }
+
+    /**
+     * Returns true if there are no more lines to read from the stream.
+     */
+    bool empty()
+    {
+        return _empty;
+    }
+}
+
+/**
+ * Convenience function for returning a delimiter range.
+ */
+@property auto byDelimiter(T = char, Delimiter)
+    (Source source, Delimiter delimiter)
+{
+    return ByDelimiter!(T, Delimiter)(source, delimiter);
+}
+
+/**
+ * Convenience function for returning a delimiter range that iterates over
+ * lines.
+ */
+@property auto byLine(T)(Source source)
+{
+    return ByDelimiter!(T, dchar)(source, '\n');
+}
+
+version (unittest)
+{
+    void testByDelimiter(const string[] lines, string delimiter)
+    {
+        import io.file.temp;
+        import std.array : join;
+        import std.algorithm : equal;
+
+        immutable text = lines.join(delimiter);
+
+        auto f = tempFile();
+        f.writeExactly(text);
+        f.position = 0;
+
+        assert(f.byDelimiter(delimiter).equal(lines));
+        assert(f.position == text.length);
+
+        // Add a trailing terminator at the end of the file.
+        assert(f.write(delimiter) == delimiter.length);
+        f.position = 0;
+        assert(f.byDelimiter(delimiter).equal(lines));
+    }
+}
+
+unittest
+{
+    immutable lines = [
+        "This is the first line",
+        "",
+        "That was a blank line.",
+        "This is the penultimate line!",
+        "This is the last line.",
+    ];
+
+    testByDelimiter(lines, "\n");
+    testByDelimiter(lines, "\r\n");
 }
